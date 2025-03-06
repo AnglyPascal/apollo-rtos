1a2b607be00f8acb8400be6f901b597a490079a8
AnglyPascal <ahsanalmahir@gmail.com>
2025-03-06 02:49:54 +0000

    fs unmap shared map fixed

--- include/fs/fs_impl.h
+++ include/fs/fs_impl.h
@@ -7,69 +7,81 @@
 #include "utility/debug.h"
 #include "utility/mutex.h"
 
-struct mmap_unit_t {
-  void *buf = nullptr;
-  size_t sz = 0;
-  bool pool = false;
-};
-
-template <typename desc_t>
-class _fd_t
-{
-  using inode_t = _inode_t<desc_t>;
-  using fn_t = typename desc_t::fn_t;
-
-  uint8_t w_cnt = 0;
-  uint8_t r_cnt = 0;
-
-public:
-  fn_t fn = null_fn;
-  mutex<> mtx = {"fd"};
-
-  const inode_t *inode = nullptr;
-  mmap_unit_t mu = {};
-
-  void acquire(fn_t _fn, const inode_t *_inode, bool w_en)
-  {
-    assert(fn == null_fn || fn == _fn);
-    r_cnt++;
-    w_cnt += w_en;
-    fn = _fn;
-    inode = _inode;
-  }
-
-  void release(bool w_en)
-  {
-    r_cnt--;
-    w_cnt -= w_en;
-    if (r_cnt == 0)
-      new (this) _fd_t{};
-  }
-};
-
 template <typename desc_t, desc_t desc>
 class fs_impl_t
 {
-  using fd_t = _fd_t<desc_t>;
+private:
   using inode_t = _inode_t<desc_t>;
-
   using fs_t = _fs_t<desc_t, desc>;
-  inline static fs_t fs;
 
-  inline static allocator<alloc_heap, 4> pool;
-  inline static fd_t open_files[desc.n_open_files] = {};
+  struct mmap_unit_t {
+  private:
+    bool pooled = false;
 
-  static fd_t *find_fd(fn_t fn)
-  {
-    fd_t *empty_fd = nullptr;
-    for (auto &fd : open_files) {
-      if (fd.fn == fn)
-        return &fd;
-      if (fd.fn == null_fn)
-        empty_fd = &fd;
+  public:
+    void *buf = nullptr;
+    size_t sz = 0;
+
+    void mmap(size_t _sz)
+    {
+      if (buf != nullptr)
+        return;
+
+      buf = pool.alloc(_sz);
+      sz = _sz;
+      pooled = true;
     }
-    return empty_fd;
-  }
+
+    void mmap(uint8_t *_buf, size_t _sz)
+    {
+      unmap();
+      buf = _buf;
+      sz = _sz;
+      pooled = false;
+    }
+
+    void unmap()
+    {
+      if (pooled)
+        pool.dealloc((byte_t *)buf);
+      new (this) mmap_unit_t{};
+    }
+  };
+
+  class fd_t
+  {
+    using inode_t = _inode_t<desc_t>;
+    using fn_t = typename desc_t::fn_t;
+
+    uint8_t w_cnt = 0;
+    uint8_t r_cnt = 0;
+
+  public:
+    fn_t fn = null_fn;
+    mutex<> mtx = {"fd"};
+
+    const inode_t *inode = nullptr;
+    mmap_unit_t mu = {};
+
+    uint8_t ref_cnt() const { return r_cnt; }
+
+    void acquire(fn_t _fn, const inode_t *_inode, bool w_en)
+    {
+      assert(fn == null_fn || fn == _fn);
+      r_cnt++;
+      w_cnt += w_en;
+      fn = _fn;
+      inode = _inode;
+    }
+
+    void release(bool w_en)
+    {
+      r_cnt--;
+      w_cnt -= w_en;
+      if (r_cnt == 0)
+        new (this) fd_t{};
+    }
+  };
 
 public:
   class file_t
@@ -110,29 +122,19 @@ public:
     auto ft() const { return fd->inode->flag.ft(); }
     auto fsz() const { return fd->inode->fsz(); }
 
-    void *mmap(size_t buf_sz)
+    void *mmap(size_t sz)
     {
       auto &tmu = target_mu();
-      if (tmu.buf != nullptr)
-        return tmu.buf;
-
-      tmu.buf = pool.alloc(buf_sz);
-      tmu.sz = buf_sz;
-      tmu.pool = true;
-
+      tmu.mmap(sz);
       load();
       return tmu.buf;
     }
 
-    void mmap(uint8_t *buf, size_t buf_sz)
+    void mmap(uint8_t *buf, size_t sz)
     {
       unmap();
-
       auto &tmu = target_mu();
-      tmu.buf = buf;
-      tmu.sz = buf_sz;
-      tmu.pool = false;
-
+      tmu.mmap(buf, sz);
       load();
     }
 
@@ -150,19 +152,20 @@ public:
 
     void unmap()
     {
-      auto &tmu = target_mu();
-      if (tmu.pool)
-        pool.dealloc((byte_t *)tmu.buf);
+      if (!mmap_shared) {
+        mu.unmap();
+        return;
+      }
 
-      tmu.buf = nullptr;
-      tmu.sz = 0;
-      tmu.pool = false;
+      // last reference to fd
+      if (fd->ref_cnt() == 1)
+        fd->mu.unmap();
     }
 
     void load()
     {
       auto &tmu = target_mu();
-      assert_dump(tmu.buf != nullptr, "%d\r\n", fd->fn);
+      assert(tmu.buf != nullptr, "%d\r\n", fd->fn);
       lock_guard guard{fd->mtx};
       fs.load(fd->inode, (uint8_t *)tmu.buf, tmu.sz);
     }
@@ -175,6 +178,19 @@ public:
       fs.store(fd->inode, (uint8_t *)tmu.buf, tmu.sz);
     }
   };
+
+private:
+  static fd_t *find_fd(fn_t fn)
+  {
+    fd_t *empty_fd = nullptr;
+    for (auto &fd : open_files) {
+      if (fd.fn == fn)
+        return &fd;
+      if (fd.fn == null_fn)
+        empty_fd = &fd;
+    }
+    return empty_fd;
+  }
 
 public:
   static file_t open(fn_t fn, size_t sz, uint32_t flags)
@@ -215,4 +231,9 @@ public:
   }
 
   static void trace() { fs.trace(is_open); }
+
+private:
+  inline static fs_t fs;
+  inline static allocator<alloc_heap, 4> pool;
+  inline static fd_t open_files[desc.n_open_files] = {};
 };
