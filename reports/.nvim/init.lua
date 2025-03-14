6535ed57c072ea7c82c5a2dee89e82b806afa663
AnglyPascal <ahsanalmahir@gmail.com>
2025-03-14 01:43:59 +0000

    report progress

--- reports/.nvim/init.lua
+++ reports/.nvim/init.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("User", {
   pattern = "VimtexEventCompileSuccess",
   callback = function()
-    vim.fn.jobstart("texcount -inc -v -sum main.tex | grep 'Sum count' | tail -n 1 | awk '{print $3}' > wordcount.tex", { detach = true })
+    vim.fn.jobstart("texcount -inc -nobib -v -sum=1,1,0,1,0,0,0 main.tex | grep 'Sum count' | tail -n 1 | awk '{print $3}' > wordcount.tex", { detach = true })
   end,
 })
